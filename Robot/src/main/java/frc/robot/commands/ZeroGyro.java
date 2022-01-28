24e2d3c5c7ad76d7ae33421f2063af9a8d48c328
Geoff Schmit <gcschmit@gmail.com>
2022-01-27 20:32:17 -0600

    added development version of swerve-template

---
+++ Robot/src/main/java/frc/robot/commands/ZeroGyro.java
@@ -0,0 +1,51 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+    
+/**
+ *
+ */
+public class ZeroGyro extends CommandBase {
+
+        private final DrivetrainSubsystem m_drivetrainSubsystem;
+ 
+
+    public ZeroGyro(DrivetrainSubsystem subsystem) {
+
+        m_drivetrainSubsystem = subsystem;
+        addRequirements(m_drivetrainSubsystem);
+
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        m_drivetrainSubsystem.zeroGyroscope();
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public boolean runsWhenDisabled() {
+        
+        return false;
+
+    
+    }
+}
