b8466352d76c2f0098b4e9bc5a8a222b1f2ddd1f
Daniel Wu <54150946+daniel-n6@users.noreply.github.com>
2022-02-19 10:47:09 -0600

    added followpath cmd

---
+++ Robot/src/main/java/frc/robot/commands/FollowPath.java
@@ -0,0 +1,26 @@
+package frc.robot.commands;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+/**
+ * 
+ *
+ */
+public class FollowPath extends PPSwerveControllerCommand {
+
+    public FollowPath(PathPlannerTrajectory trajectory, DrivetrainSubsystem subsystem) {
+        super(trajectory, subsystem::getPose, subsystem.getKinematics(), new PIDController(1, 1, 0),
+                new PIDController(1, 1, 0), new ProfiledPIDController(1, 1, 0, new Constraints(1, 1)),
+                subsystem::setSwerveModuleStates, subsystem);
+        addRequirements(subsystem);
+    }
+}
