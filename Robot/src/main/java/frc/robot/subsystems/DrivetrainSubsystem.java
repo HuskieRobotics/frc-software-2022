9e955e84aad7665f45710450710359244eaac7ca
Aayush Modi <ambase6@gmail.com>
2022-02-19 14:31:56 -0600

    added straight path

--- Robot/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ Robot/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -113,6 +113,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
                                 -DRIVETRAIN_WHEELBASE_METERS / 2.0);
 
                 m_pigeon.setYaw(0.0);
+                m_pigeon.setFusedHeading(0.0);
 
                 // There are 4 methods you can call to create your swerve modules.
                 // The method you use depends on what motors you are using.
