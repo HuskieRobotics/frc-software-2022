c2f396c189676dfd2b2df940bac68b167cf13f09
Geoff Schmit <gcschmit@gmail.com>
2022-03-05 17:38:58 -0600

    move resetOdometry to initialize

--- Robot/src/main/java/frc/robot/commands/FollowPath.java
+++ Robot/src/main/java/frc/robot/commands/FollowPath.java
@@ -13,14 +13,30 @@ import static frc.robot.Constants.*;
  *
  */
 public class FollowPath extends PPSwerveControllerCommand {
+    private ProfiledPIDController thetaController;
+    private DrivetrainSubsystem drivetrainSubsystem;
+    private PathPlannerTrajectory trajectory;
+
     public FollowPath(PathPlannerTrajectory trajectory, ProfiledPIDController thetaController, DrivetrainSubsystem subsystem) {
         super(trajectory, subsystem::getPose, subsystem.getKinematics(), new PIDController(AutoConstants.kPXController, 0, 0),
                 new PIDController(AutoConstants.kPYController, 0, 0), thetaController,
                 subsystem::setSwerveModuleStates, subsystem);
-        addRequirements(subsystem);
+
+        this.thetaController = thetaController;
+        this.drivetrainSubsystem = subsystem;
+        this.trajectory = trajectory;
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+
+        // reset the theta controller such that old accumuldated ID values aren't used with the new path
+        //      this doesn't matter if only the P value is non-zero, which is the current behavior
+        this.thetaController.reset(this.drivetrainSubsystem.getPose().getRotation().getRadians());
 
         // Reset odometry to the starting pose of the trajectory.
-        subsystem.resetOdometry(trajectory.getInitialState());
+        this.drivetrainSubsystem.resetOdometry(this.trajectory.getInitialState());
 
     }
 
