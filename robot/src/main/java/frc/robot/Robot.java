15620edeee55b17b1485b25414ac1bba9b85bf40
Geoff Schmit <gcschmit@gmail.com>
2022-03-08 11:14:12 -0600

    removed warnings

--- robot/src/main/java/frc/robot/Robot.java
+++ robot/src/main/java/frc/robot/Robot.java
@@ -14,10 +14,8 @@ package frc.robot;
 
 import edu.wpi.first.hal.FRCNetComm.tInstances;
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
