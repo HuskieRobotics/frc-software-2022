c130e7ef5da6e72b70201647a7b6392022f4f9a6
Geoff Schmit <gcschmit@gmail.com>
2022-03-23 23:06:25 -0500

    move secondary arms in to dampen swing

--- Robot/src/main/java/frc/robot/commands/ExtendClimberBeforeNextRungCommand.java
+++ Robot/src/main/java/frc/robot/commands/ExtendClimberBeforeNextRungCommand.java
@@ -3,13 +3,17 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.ElevatorConstants;
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.SecondaryArm;
 
 public class ExtendClimberBeforeNextRungCommand extends CommandBase {
     private final Elevator m_elevator;
+    private final SecondaryArm m_secondMechanism;
 
-    public ExtendClimberBeforeNextRungCommand(Elevator subsystem) {
-        m_elevator = subsystem;
+    public ExtendClimberBeforeNextRungCommand(Elevator elevator, SecondaryArm secondaryArm) {
+        m_elevator = elevator;
+        m_secondMechanism = secondaryArm;
         addRequirements(m_elevator);
+        addRequirements(m_secondMechanism);
     }
 
     @Override
@@ -19,6 +23,11 @@ public class ExtendClimberBeforeNextRungCommand extends CommandBase {
     @Override
     public void execute() {
         m_elevator.setElevatorMotorPosition(ElevatorConstants.REACH_JUST_BEFORE_NEXT_RUNG, true);
+
+        // if the robot has been transferred from the elevator to secondary arms, move the secondary arms in to dampen the swing
+        if(m_elevator.hasTransferredToSecondary()) {
+            m_secondMechanism.moveSecondaryArmIn();
+        }
     }
 
     @Override
