cfdee076cf24baffae3a441738ad45dff2ec0313
Driver Station <ambase6@gmail.com>
2022-03-23 20:52:43 -0500

    adjusted heights and created constant for slow

--- Robot/src/main/java/frc/robot/commands/ReachToNextRungCommand.java
+++ Robot/src/main/java/frc/robot/commands/ReachToNextRungCommand.java
@@ -23,7 +23,7 @@ public class ReachToNextRungCommand extends CommandBase {
     @Override
     public void execute() {
         if(m_elevator.isTimeToExtend()) {
-            m_elevator.setElevatorMotorPosition(ElevatorConstants.REACH_TO_NEXT_RUNG_HEIGHT, false);
+            m_elevator.setElevatorMotorPosition(269733, false);
         }
 
         // if the robot has been transferred from the elevator to secondary arms, move the secondary arms in to dampen the swing
