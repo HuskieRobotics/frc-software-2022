1711e6892da9edcd829244da72888f82f49bcaa3
Geoff Schmit <gcschmit@gmail.com>
2022-04-04 16:12:06 -0500

    add indicator

--- Robot/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ Robot/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -203,6 +203,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
                 this.feedForward = new SimpleMotorFeedforward(AutoConstants.ksVolts,
                                 AutoConstants.kvVoltSecondsPerMeter, AutoConstants.kaVoltSecondsSquaredPerMeter);
 
+                tabMain.addBoolean("Current Limiting", () -> this.limitCurrentDraw);
                 tabMain.addNumber("Limelight Vel", () -> getVelocityFromLimelight());
                 tabMain.addBoolean("Launchpad Dist", () -> isAtLaunchpadDistance());
                 tabMain.addBoolean("Wall Dist", () -> isAtWallDistance());
