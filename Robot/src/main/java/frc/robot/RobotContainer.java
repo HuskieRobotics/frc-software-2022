aaaae2fe1d26b43c9f1be27d24670b4272ae8ba5
Geoff Schmit <gcschmit@gmail.com>
2022-04-15 13:10:47 -0500

    prepare for tuning auto climb

--- Robot/src/main/java/frc/robot/RobotContainer.java
+++ Robot/src/main/java/frc/robot/RobotContainer.java
@@ -2,15 +2,18 @@ package frc.robot;
 
 import static frc.robot.Constants.JoystickConstants.*;
 
+import java.util.Map;
 
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.networktables.EntryListenerFlags;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.EventImportance;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -134,6 +137,14 @@ public class RobotContainer {
     // Configure autonomous sendable chooser
 
     configureAutoCommands();
+
+    Shuffleboard.getTab("MAIN").add("retract delay", ElevatorConstants.RETRACT_DELAY_AFTER_EXTENSION_UNDER_RUNG)
+                                .withWidget(BuiltInWidgets.kNumberSlider)
+                                .withProperties(Map.of("min", 0.0, "max", 0.20)) // specify widget properties here
+                                .getEntry()
+                                .addListener(event -> {
+                                  ElevatorConstants.RETRACT_DELAY_AFTER_EXTENSION_UNDER_RUNG = event.getEntry().getValue().getDouble();
+                                }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
 
     if(TUNING) {
       Shuffleboard.getTab("Elevator").add("Reach to Next Rung", new ReachToNextRungCommand(m_elevator, m_secondMechanism));
@@ -317,7 +328,7 @@ public class RobotContainer {
             new InstantCommand(() -> m_secondMechanism.moveSecondaryArmOut(), m_secondMechanism),
             new WaitCommand(0.5), // wait for secondary arm to be positioned
             new ReachToNextRungWithPitchCommand(m_elevator, m_secondMechanism),
-            new WaitCommand(0.040), // wait for secondary arm to be positioned; FIXME tune for half the period of swing
+            new WaitCommand(ElevatorConstants.RETRACT_DELAY_AFTER_EXTENSION_UNDER_RUNG), // wait for secondary arm to be positioned
             new RetractClimberMinimumCommand(m_elevator)));
 
     // configure climb to 1/2 (low/mid) rung climb sequence
